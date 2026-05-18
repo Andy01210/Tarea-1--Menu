6929f1e7cdea54191e0175c6eca590bba01c03e2
Andy01210 <andy_paulino@ucne.edu.do>
2026-05-18 00:05:16 -0400

    Agregadas la opcion 1,2 y 3 del menu

---
+++ Tarea1.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+
+public class Tarea1{
+    
+    static Scanner leer = new Scanner(System.in); 
+    public static void main(){
+       
+        int op = 0;
+        while(op != 11){
+            System.out.println("Elija la opcion que desea realizar: \n 1.Calculadora\n 2.Numero par o impar \n 3.Tabla de multiplicar\n "+
+            "5.promedio de notas\n 6.Clase Estudiante\n 7. Cuenta Bancaria\n 8.Herencia vehiculo\n 9.Matriz 3x2\n 10.SistemaBasico de inventario\n 11.Salir del menu"
+            );
+            op = leer.nextInt();
+            switch (op){
+                case 1:
+                    System.out.println("Ingrese el primer numero a operar");
+                    double num1 = leer.nextDouble();
+                    System.out.println("Elija la operacion que desea realizar: \n 1.Suma \n 2.Resta \n 3.Multiplicacion\n 4.Division");
+                    int opSuma = leer.nextInt();
+                    System.out.println("Ingrese el segundo numero a operar");
+                    double num2 = leer.nextDouble();
+                    double resultado = 0;           
+                    switch(opSuma){
+                        case 1:
+                         resultado = num1 + num2;
+                         System.out.println("El resultado de su operacion fue:"+ resultado);
+                         break;
+                        case 2:
+                            resultado = num1 - num2;
+                            System.out.println("El resultado de su operacion fue:"+ resultado);
+                            break;
+                        case 3:
+                            resultado = num1 * num2;
+                            System.out.println("El resultado de su operacion fue:"+ resultado);
+                            break;
+                        case 4:
+                            resultado = num1 / num2;
+                            System.out.println("El resultado de su operacion fue: "+ resultado);
+                            break;
+                        default:
+                            System.out.println("opcion no valida");
+                            break;
+
+                        }
+                     Cls();
+                     break; 
+                case 2:
+                    System.out.println("ingrese un numero");
+                    int a = leer.nextInt();
+                    if(a % 2 == 0){
+                        System.out.println("El numero ingresado es par");
+                    }else
+                        {System.out.println("El numero ingresado es impar");}
+                    Cls();
+                    break;
+                case 3:
+                    System.out.println("Ingrese el numero del cual desea ver su tabla");
+                    int b = leer.nextInt();
+                    System.out.println("La tabla del numero ingresado es la siguiente: \n"
+                    +b +" x 1 ="+ b*1 +"\n"
+                    +b +" x 2 ="+ b*2+"\n"
+                    +b +" x 3 ="+ b* 3+"\n"
+                    +b +" x 4 ="+ b*4+"\n"
+                    +b +" x 5 ="+ b*5+"\n"
+                    +b +" x 6 ="+ b*6+"\n"
+                    +b +" x 7 ="+ b*7 +"\n"
+                    +b +" x 8 ="+ b*8+"\n"
+                    +b +" x 9 ="+ b*9 +"\n"
+                    +b +" x 10 ="+ b*10 );
+                    Cls();
+                    break;
+                case 11:
+                    System.out.println("Gracias por participar");
+                    break;
+                default:
+                    System.out.println("Opcion fuera de rango, intente nuevamente");
+                    Cls();
+    }
+}
+    }
+
+
+
+
+
+
+static void pausa(){
+    System.out.println("\n\nPresione Enter para continuar");
+    leer.nextLine(); 
+    leer.nextLine();
+     
+
+}
+static void Cls(){
+    pausa();
+    for(int i =0; i < 50; i++){
+                System.out.println(" ");
+            }
+
+}
+}
